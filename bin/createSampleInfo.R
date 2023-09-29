642a713486b49ee27d0173b9a36161927316b094
phamhoanggiang262 <phamhoanggiang262@gmail.com>
2023-09-29 16:45:31 +0700

    update pipeline

--- bin/createSampleInfo.R
+++ bin/createSampleInfo.R
@@ -2,7 +2,7 @@
 
 library(optparse)
 library(readr)
-library(tidyverse)
+
 
 option_list <- list(
   make_option(
@@ -21,21 +21,27 @@ opt_parser <- OptionParser(option_list = option_list);
 opt <- parse_args(opt_parser);
 
 # read sample sheet file
+
 samplesheet <- read.csv(opt$samplesheet)
 
 #rename column name
-samplesheet <- rename(samplesheet, FileName = fastq_1)
+colnames(samplesheet)[2] = "FileName"
+
+
 
 #split file name from path
-samplesheet$FileName <- basename(samplesheet$FileName)
+#samplesheet$FileName <- basename(samplesheet$FileName)
+samplesheet$FileName<- sub('.*/', '', samplesheet$FileName)
 
 #set to absolute path
-samplesheet$FileName <- file.path(opt$path, paste0(samplesheet$SampleName, ".bam"))
+samplesheet$FileName <- file.path(opt$path, paste0(samplesheet$sample, ".bam"))
 
 
 #move FileName in the front
-samplesheet %>%  relocate(FileName, .before = SampleName) -> samplesheet
+#samplesheet <- relocate(samplesheet$FileName, .before = SampleName) 
 
+
+samplesheet <- subset(samplesheet, select = c(2,1,3))
 write.table(samplesheet, file="sampleInfo.csv", sep=",", row.names = F, col.names = T)
 
 
